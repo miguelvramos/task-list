c7cec0e1d70938d27935dfb26b3a280fdb7bc87c
Miguel Ramos <miguelvieiraramos02@gmail.com>
2019-10-23 23:48:55 -0300

    retirando comentarios

--- app.js
+++ app.js
@@ -17,22 +17,14 @@ function addTask(e) {
     if(taskInput.value === ''){
         alert('Empty');
     }else{
-        //Create li element
         const li = document.createElement('li');
-        //Add class
         li.className = 'collection-item';
-        //Append node to li
         li.appendChild(document.createTextNode(taskInput.value));
 
-        //Create link element
         const link = document.createElement('a');
-        //Add class
         link.className = 'delete-item secondary-content';
-        //Add icon html
         link.innerHTML = '<i class="fa fa-remove"></li>';
-        //Append link to li
         li.appendChild(link)
-        //Append li to ul
         taskList.appendChild(li);        
 
         taskInput.value = '';
@@ -65,7 +57,5 @@ function filterTasks(e) {
         }else{
             task.style.display = 'none';
         }
-        
     });
-
 }
