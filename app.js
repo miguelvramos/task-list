cafde9667803fb304a62fd580a2d198e4d6058f9
Miguel Ramos <miguelvieiraramos02@gmail.com>
2019-10-23 23:38:28 -0300

    novas funcionalidades

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
 const taskInput = document.querySelector('#task');
-const clearBtn = document.querySelector('.clear-task');
+const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 
 loadAllEventListeners();
 
 function loadAllEventListeners() {
-    form.addEventListener('submit', function(e){
-        if(taskInput.value === ''){
-            alert('Empty');
-        }
-        
+    form.addEventListener('submit', addTask);
+    taskList.addEventListener('click', removeTask);
+    clearBtn.addEventListener('click', clearTasks);
+    filter.addEventListener('keyup', filterTasks);
+} 
+
+function addTask(e) {
+    if(taskInput.value === ''){
+        alert('Empty');
+    }else{
         //Create li element
         const li = document.createElement('li');
         //Add class
@@ -31,6 +36,44 @@ function loadAllEventListeners() {
         taskList.appendChild(li);        
 
         taskInput.value = '';
-        e.preventDefault();
+    }
+    e.preventDefault();
+}
+
+function removeTask(e) {
+    if(e.target.className == 'fa fa-remove'){
+        if(confirm('Are you sure?')){
+        e.target.parentElement.parentElement.remove();  
+        }
+    }
+}
+
+function clearTasks(e) {
+    document.querySelectorAll('.collection-item').forEach(function(item){
+        item.remove();
     });
-} 
+}
+
+function filterTasks(e) {
+    let text = filter.value.toLowerCase();
+    const tasks = document.querySelectorAll('.collection-item');
+    
+    tasks.forEach(function(task){
+        const item = task.firstChild.textContent.toLowerCase();
+        if(item.indexOf(text) != -1){
+            task.style.display = 'block';
+        }else{
+            task.style.display = 'none';
+        }
+        
+    });
+
+}
+
+/*
+pegar o texto
+verificar se existe na lista de itens
+mostrar o valor se existir
+omitir se nao existir
+
+*/
